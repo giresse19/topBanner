2ee4220a0bfe59f74c0b9f216f5a77799277e797
giresse19 <ajanohpelkins18@gmail.com>
2024-11-11 12:36:18 +0200

    feat: working version

--- TopBanner/Form1.cs
+++ TopBanner/Form1.cs
@@ -3,51 +3,66 @@ using System.Diagnostics;
 
 namespace TopBanner;
 
-public partial class Form1 : Form
-{
-    public Form1()
-    { 
+public partial class Form1 : Form {
+    private LinkLabel linkLabel;
+   // private Timer timer;
+    
+    public Form1() { 
         InitializeComponent();
-        
-     // Set form properties
-     this.FormBorderStyle = FormBorderStyle.None;
-     this.TopMost = true;
-     this.StartPosition = FormStartPosition.Manual;
-     this.Width = Screen.PrimaryScreen.WorkingArea.Width;
-     this.Height = 10;
-     this.BackColor = Color.SkyBlue;
-     this.Location = new Point(0, 0);  // Position at the top of the screen
+        this.FormBorderStyle = FormBorderStyle.None;
+        this.TopMost = true;
+        this.StartPosition = FormStartPosition.Manual;
+        this.Width = Screen.PrimaryScreen.WorkingArea.Width;
+        this.Height = 10;
+        this.BackColor = Color.SkyBlue;
+        this.Location = new Point(0, 0);
+
+        SetupBanner();
+    // StartUrlPolling();
     }
     
-    private void BannerForm_Load(object sender, EventArgs e)
-    {
-        LinkLabel linkLabel = new LinkLabel();
+    private void SetupBanner() { 
+        linkLabel = new LinkLabel();
                
-        linkLabel.BackColor = Color.Red; 
-        linkLabel.Text = "I am a Dynamic LinkLabel"; 
-        linkLabel.Name = "DynamicLinkLabel";
-        linkLabel.LinkColor = Color.Red;
-        linkLabel.AutoSize = false;
-        linkLabel.Font = new Font("Georgia", 16); 
+        linkLabel.Name = "Paperfree";
+        linkLabel.Text = "Return to Paperfree"; 
+        linkLabel.Font = new Font("Arial", 12, FontStyle.Bold); 
+        linkLabel.AutoSize = true;
+        linkLabel.LinkColor = Color.Navy;
+        linkLabel.BackColor = Color.Transparent;
+        linkLabel.LinkBehavior = LinkBehavior.HoverUnderline;
         
-       // Center the LinkLabel horizontally within the banner
-      linkLabel.Location = new Point((this.Width - linkLabel.Width) / 2, (this.Height - linkLabel.Height) / 2);
-       linkLabel.Click += LinkLabel_Click;
-       
-       this.Controls.Add(linkLabel);
+        linkLabel.Click += LinkLabel_Click ;
+          
+        this.Controls.Add(linkLabel);
+        
+        CenterLinkLabel();
+        
+        this.PerformLayout();
+        this.Refresh();
     }
     
-    // Open a URL when the link label is clicked
-    private void LinkLabel_Click(object sender, EventArgs e)
-    {
-        string url = "https://www.seb.ee"; // Replace with your desired URL
+    private void CenterLinkLabel() {
+        linkLabel.Location = new Point(
+            (this.ClientSize.Width - linkLabel.Width) / 2,
+            (this.ClientSize.Height - linkLabel.Height) / 2
+        );
+    }
+    
+    protected override void OnLoad(EventArgs e) {
+        base.OnLoad(e);
+        CenterLinkLabel();
+        this.PerformLayout();
+        this.Refresh();
+    }
+    
+    private void LinkLabel_Click(object sender, EventArgs e) {
+        string url = "https://www.example.com"; // Replace with your desired URL
         Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
     }
     
-    // Prevent closing of form
-    protected override void OnFormClosing(FormClosingEventArgs e)
-    {
-        e.Cancel = true; // Prevent the form from being closed
+    protected override void OnFormClosing(FormClosingEventArgs e) {
+        e.Cancel = true; // Prevent form closing
     }
     
 }
